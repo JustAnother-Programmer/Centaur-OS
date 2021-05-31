7c63092bd0878067e9806f1c141f5dced487edbb
Rory <mlggamer78123@gmail.com>
2021-05-31 21:08:39 +0100

    Add makeifle, testing loading from disc

--- boot.asm
+++ boot.asm
@@ -10,20 +10,6 @@ _start:
 start:
     jmp 0x7c0:step2
 
-handle_zero:
-    mov ah, 0eh
-    mov al, 'Z'
-    mov bx, 0x00
-    int 0x10
-    iret
-
-handle_one:
-    mov ah, 0eh
-    mov al, 'B'
-    mov bx, 0x00
-    int 0x10
-    iret
-
 step2:
     cli
     mov ax, 0x7c0
@@ -34,15 +20,22 @@ step2:
     mov sp, 0x7c00
     sti
 
-    mov word[ss:0x00], handle_zero
-    mov word[ss:0x02], 0x7c0
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
+    jc error
 
-    mov word[ss:0x04], handle_one
-    mov word[ss:0x06], 0x7c0
+    mov si, buffer
+    call print
 
-    int 1
+    jmp $
 
-    mov si, message
+error:
+    mov si, error_message
     call print
     jmp $
 
@@ -62,7 +55,9 @@ print_char:
     int 0x10
     ret
 
-message: db 'Hello World!', 0
+error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
