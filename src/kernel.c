7367ce6584fb4e9106c6b00d990d1f7387b4ec04
Rory <mlggamer78123@gmail.com>
2021-07-27 17:45:50 +0100

    Add kernel panic

--- src/kernel.c
+++ src/kernel.c
@@ -71,6 +71,12 @@ void print(const char* str)
 
 static struct paging_4gb_chunk* kernel_chunk = 0;
 
+void panic(const char* msg)
+{
+    print(msg);
+    while(1) {}
+}
+
 void kernel_main()
 {
     terminal_init();
